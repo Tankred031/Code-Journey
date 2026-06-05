1880157347332f69b1e0f57357ed8c9cb4842df2
Tankred031 <tankred.kralj@gmail.com>
2026-06-05 19:04:38 +0200

    JS dio 1

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log(projekti);
+//console.log(projekti);
+
+const projectsList = document.querySelector(".projects-section");
+
+console.log(projectsList);
+
+function renderProjects() {
+    projekti.forEach(function(projekt) {
+        console.log(projekt.naziv);
+    });    
+}
+
+renderProjects()
